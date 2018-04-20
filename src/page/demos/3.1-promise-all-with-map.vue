7e4a3d79a204534550a9d1121f410d3ebf4caee6
李杨 <1711467488@qq.com>
2018-04-20 23:08:46 +0800

    no message

---
+++ src/page/demos/3.1-promise-all-with-map.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <h1>使用 map 优化 promise.all</h1>
+    <el-button @click="start">同时向3个接口请求数据</el-button>
+    <p v-for="(row, index) in value" :key="index">{{row}}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  groupName: '进阶用法',
+  title: '使用 map 优化 promise.all',
+  data () {
+    return {
+      value: ''
+    }
+  },
+  methods: {
+    start () {
+      // 请对比 src/page/demos/2.4-promise-all.vue
+      Promise.all([1, 2, 3].map(e => {
+        return new Promise(resolve => {
+          setTimeout(resolve, e * 1000, `data from data${e}`)
+        })
+      }))
+        .then(res => {
+          this.value = res
+        })
+    }
+  }
+}
+</script>
---
+++ src/page/demos/3.2-promise-queue-with-reduce.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <h1>promise + reduce 实现队列</h1>
+    <p>执行五个动作，分别间隔 1s 2s 3s 4s</p>
+    <p>[ 打印1 ] - 间隔1s - [ 打印2 ] - 间隔2s - [ 打印3 ] - 间隔3s - [ 打印4 ] - 间隔4s - [ 打印5 ]</p>
+    <el-button @click="start">开始</el-button>
+    <p>{{value.join(' - ')}}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  groupName: '进阶用法',
+  title: 'promise + reduce 实现队列',
+  data () {
+    return {
+      value: []
+    }
+  },
+  methods: {
+    start () {
+      [0, 1, 2, 3, 4].reduce((p, e) => p.then(() => new Promise(resolve => {
+        setTimeout(() => {
+          this.value.push(e + 1)
+          resolve()
+        }, e * 1000)
+      })), Promise.resolve())
+    }
+  }
+}
+</script>
