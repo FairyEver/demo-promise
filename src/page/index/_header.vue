6b1fee174341c6cdac562f8ab5143293a45acabc
李杨 <1711467488@qq.com>
2018-04-20 20:43:25 +0800

    no message

---
+++ src/page/index/_header.vue
@@ -0,0 +1,14 @@
+<template>
+  <el-menu
+    class="x-header"
+    mode="horizontal"
+    :router="true">
+    <el-menu-item index="/">首页</el-menu-item>
+    <el-submenu index="2">
+      <template slot="title">我的工作台</template>
+      <el-menu-item index="2-1">选项1</el-menu-item>
+      <el-menu-item index="2-2">选项2</el-menu-item>
+      <el-menu-item index="2-3">选项3</el-menu-item>
+    </el-submenu>
+  </el-menu>
+</template>
---
+++ src/page/index/index.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="x-index">
+    <IndexHeader></IndexHeader>
+    <div class="x-main"></div>
+  </div>
+</template>
+
+<script>
+export default {
+  components: {
+    IndexHeader: () => import('./_header.vue')
+  }
+}
+</script>
+
+<style lang="scss">
+.x-index {
+  display: flex;
+  height: 100%;
+  flex-direction: column;
+  .x-header {
+    width: 100%;
+  }
+  .x-main {
+    width: 100%;
+    flex-grow: 1;
+  }
+}
+</style>
