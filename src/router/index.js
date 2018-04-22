ea9963ca76885aa3497a1fcc5b9231d78b98ea44
李杨 <1711467488@qq.com>
2018-04-22 21:37:03 +0800

    no message

--- src/router/index.js
+++ src/router/index.js
@@ -4,29 +4,29 @@ import path from 'path'
 
 Vue.use(Router)
 
-// const req = require.context('@/page/demos', false, /\.vue$/)
-// const demos = req.keys().map(req).map(e => {
-//   const def = e.default
-//   const name = path.basename(def.__file, '.vue')
-//   def.routerPath = name
-//   def.routerName = name
-//   return def
-// })
-
-// 下面这是上面写法的拆分 便于理解
-const req = require.context('@/page/demos', false, /\.vue$/) // 能被 require 请求到的文件的上下文
-console.log(req)
-const keys = req.keys() // 每个文件的路径
-console.log(keys)
-const allFile = keys.map(key => req(key)) // 相当于依次require了每个文件后组成一个数组
-console.log(allFile)
-const demos = allFile.map(e => {
+const req = require.context('@/page/demos', false, /\.vue$/)
+const demos = req.keys().map(req).map(e => {
   const def = e.default
   const name = path.basename(def.__file, '.vue')
   def.routerPath = name
   def.routerName = name
   return def
 })
+
+// 下面这是上面写法的拆分 便于理解
+// const req = require.context('@/page/demos', false, /\.vue$/) // 能被 require 请求到的文件的上下文
+// console.log(req)
+// const keys = req.keys() // 每个文件的路径
+// console.log(keys)
+// const allFile = keys.map(key => req(key)) // 相当于依次require了每个文件后组成一个数组
+// console.log(allFile)
+// const demos = allFile.map(e => {
+//   const def = e.default
+//   const name = path.basename(def.__file, '.vue')
+//   def.routerPath = name
+//   def.routerName = name
+//   return def
+// })
 
 export const router = new Router({
   routes: [
